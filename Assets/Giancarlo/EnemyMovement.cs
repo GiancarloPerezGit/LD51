91baa4ffc56fca4639ae6e2ba7718482e10dd77b
PetersonNormil <petersonnormil@gmail.com>
2022-10-03 06:46:54 -0400

    enemy movement and spawning in waves system

--- Assets/Giancarlo/EnemyMovement.cs
+++ Assets/Giancarlo/EnemyMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private float speed = 0.01f;
+    private float speed = 0.005f;
     // Start is called before the first frame update
     private bool spawnMove = false;
     private Vector3 target;
--- Assets/TargetDummy.cs
+++ Assets/TargetDummy.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetDistanceUp : MonoBehaviour
+public class TargetDummy : MonoBehaviour
 {
+    //public TargetType targetType; 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +16,11 @@ public class TargetDistanceUp : MonoBehaviour
     {
         
     }
+}
+
+public enum TargetType
+{
+    
+
+
 }
